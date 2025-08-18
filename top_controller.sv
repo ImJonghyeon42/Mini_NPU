f432522fb7bc5e09d0329bb05d5230378433cca5
ImJonghyeon42 <dalwhdgus@gmail.com>
2025-08-18 17:18:14 +0900

    '과거 위치 기반' 차선 선택 알고리즘

--- top_controller.sv
+++ top_controller.sv
@@ -20,10 +20,20 @@ module top_controller(
 	logic signed [17:0] result_data [0:29];
 	
 	parameter signed [17:0] THRESHOLD = 100;// 차선으로 인식할 최소 선명도 (노이즈 제거용)
-	logic [7:0] peak1_pos, peak2_pos;
-	logic signed [17:0] peak1_val, peak2_val;
+	localparam MAX_PEAKS = 4;
 	
-	enum logic [3:0] {IDLE, RECEIVE_DATA, COMPUTE, FIND_LANES, CALC_CENTER, SEND_RESULT} state;
+	logic [7:0] peak_positions [0:MAX_PEAKS -1];
+	logic signed [17:0] peak_values [0 : MAX_PEAKS -1];
+	logic [$clog2(MAX_PEAKS+1) : 0] peak_count;
+	
+	logic [7:0] best_peak1_pos, best_peak2_pos;
+	logic [7:0] best_peak1_val, best_peak2_val;
+	
+	logic [7:0] last_center_pos;
+	
+	enum logic [3:0] {IDLE, RECEIVE_DATA, COMPUTE, FIND_LANES, SELECT_PAIR, CALC_CENTER, SEND_RESULT} state;
+	
+	logic [2:0] i, j;
 	
 	always_comb begin
 		for( int i=0;i<32;i++) flattened_pixel_data[i*8 +: 8] = pixel_row_data[i];
@@ -47,9 +57,8 @@ module top_controller(
 			tx_data <= '0;
 			pixel_row_data <= '{default: '0};
 			confidence <= '0;
-			peak1_pos <= '0;
-			peak2_pos <= '0;
-			peak1_val <= '0; peak2_val; <= '0;
+			last_center_pos <= 15;
+			peak_count <= '0;
 		end
 		else begin
 			start_signal <= '0;
@@ -76,42 +85,59 @@ module top_controller(
 					if(conv_engine_done) begin
 						state <= FIND_LANES;
 						max_val_reg <= '0;
-						peak1_val <= '0; peak2_val; <= '0;
-						peak1_pos <= '0;
-						peak2_pos <= '0;
+						peak_count <= '0;
+						for(int k=0; k<MAX_PEAKS; k++) begin
+							peak_positions[k] <= '0;
+							peak_values[k] <= '0;						
+						end
 						count <= 6'd0; // 6'd1 -> 6'd0
 					end
 				end
-				FIND_MAX : begin
-					logic signed [17:0] current_val = (result_data[count[4:0]] < 0) ? -result_data[count[4:0]] : result_data[count[4:0]];
+				FIND_LANES : begin
+					logic signed [17:0] current_val = (result_data[count] < 0) ? -result_data[count] : result_data[count];
 					
-					if(current_val > THRESHOLD) begin
-						if(count[4:0] < 15) begin
-							if(current_val > peak1_val) begin
-								peak1_val <= current_val;
-								peak1_pos <= count[4:0];
-							end
-						end
-						else begin
-							if(current_val > peak2_val) begin
-								peak2_val <= current_val;
-								peak2_pos <= count[4:0];
-							end
-						end
+					if(current_val > THRESHOLD && peak_count < MAX_PEAKS) begin
+						peak_positions[peak_count] <= count;
+						peak_values[peak_count] <= current_val;
+						peak_count <= peak_count + 1'b1;					
 					end
+					
 					if(count[4:0] == 5'd29) begin
-						state <= CALC_CENTER;
+						state <= SELECT_PAIR;
 					end
 					else begin
 						count <= count + 6'd1;
 					end
+				SELECT_PAIR : begin
+					logic [7:0] min_diff = 255;
+					best_peak1_pos <= 0; best_peak2_pos <= 0;
+					best_peak1_val <= 0; best_peak2_val <= 0;
 					
+					for (int p1 = 0; p1 < MAX_PEAKS - 1; p1 = p1 + 1) begin
+						for(int p2 = p1 + 1; p2 < MAX_PEAKS; p2 = p2+1) begin
+							if(peak_positions[p1] != '0 && peak_positions[p2] != '0) begin
+								logic [7:0] current_center = (peak_positions[p1] + peak_positions[p2]) >> 1;
+								logic [7:0] diff = (current_center > last_center_pos) ? (current_center - last_center_pos) : (last_center_pos - current_center);
+								
+								if( diff < min_diff) begin
+									min_diff = diff;
+									best_peak1_pos <= peak_positions[p1];
+									best_peak2_pos <= peak_positions[p2];
+									best_peak1_val <= peak_values[p1];
+									best_peak2_val <= peak_values[p2];
+								end
+							end
+						end
+					end
+					state <= CALC_CENTER;
+				end
+						
 				CALC_CENTER: begin
-					if(peak1_val > THRESHOLD && peak2_val > THRESHOLD) begin
-						tx_data <= (peak1_pos + peak2_pos) >> 1; //나누기 2
-						confidence <= ((peak1_val >> 1) + (peak2_val >> 1)[7:0];				
+					if(best_peak1_pos != '0) begin
+						tx_data <= (best_peak1_pos + best_peak2_pos) >> 1; //나누기 2
+						confidence <= ((best_peak1_val >> 1) + (best_peak2_val >> 1)[7:0];		
 					end else begin
-						tx_data <= 8'd0;
+						tx_data <= last_center_pos;
 						confidence <= 8'd0;
 					end
 						state <= SEND_RESULT;		
@@ -119,6 +145,7 @@ module top_controller(
 				SEND_RESULT: begin					
 					done_signal <= 1'b1;
 					state <= IDLE;
+					last_center_pos <= tx_data;
 				end
 			endcase
 		end
