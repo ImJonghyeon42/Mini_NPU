de0be87656aa7db196fac286604560a990d603e7
ImJonghyeon42 <dalwhdgus@gmail.com>
2025-08-22 16:17:41 +0900

    Activation_Function 추가

---
+++ Activation_Function.sv
@@ -0,0 +1,24 @@
+`timescale 1ns/1ps
+module Activation_Function(
+	input logic clk,
+	input logic rst,
+	input logic pixel_valid,
+	input logic signed [21:0] pixel_in,
+	output logic result_valid,
+	output logic [21:0] result_out
+);
+	logic signed [21:0] result_out_reg;
+	
+	always_ff(posedge clk) begin
+		if(rst) begin
+			result_out <= '0;
+			result_valid <= 1'b0;
+		end else begin
+			result_out <= result_out_reg;
+			result_valid <= pixel_valid;
+		end
+	end
+	
+	assign result_out_reg = (pixel_in[21] == 1'b1) ? '0 : pixel_in;
+	
+endmodule	
