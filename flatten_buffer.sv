b7249a2c50444940d0998ca8558c02b0dfb69405
JONGHENNEYV <dalwhdgus@gmail.com>
2025-08-25 17:53:26 +0900

    Parameter 추가

--- flatten_buffer.sv
+++ flatten_buffer.sv
@@ -9,5 +9,7 @@ module flatten_buffer (
 	output logic signed [21:0] result_out [0:224],
 	output logic done_signal
 );
+	parameter DATA_WIDTH = 22;
+	parameter ADDR_WIDTH = 8;  // 주소 버스의 비트 수 (2^ADDR_WIDTH >= 225 이므로, 8-bit), 2^8 = 256, so 8 bits are enough for 225 addresses
 	
 	
